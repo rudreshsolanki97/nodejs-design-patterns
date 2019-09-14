f37bd4e8ab7c58aba09e483667d8b97086124d20
rudresh <rudreshsolanki101@gmail.com>
2019-09-14 20:11:23 +0530

    add makeAdder snippet

--- essential-patterns/SyncAndAsync/main.js
+++ essential-patterns/SyncAndAsync/main.js
@@ -3,3 +3,16 @@
 // an unpredictable function or inconsistently asynchronous function
 // cannot have function behaving either synchronouslu or asynchronouly on the go.
 
+async function unpredictableFunction(arg) {
+    if (arg == true) {
+        // will be invoked synchronously
+        return "Synchronous"
+    } else {
+        // will return undefined
+        // Note: to make it synchronous use busy-waiting style
+        setTimeout(function () {
+            console.log("inside timeout callback")
+            return "Asynchronous" // will never be returned
+        }, 500)
+    }
+}
--- essential-patterns/closures/main.js
+++ essential-patterns/closures/main.js
@@ -2,10 +2,10 @@
 
 // Create Dynamic Methods & call them
 let dynamicMethods = []; // object
-dynamicMethods["printDate"] = function() {
+dynamicMethods["printDate"] = function () {
   console.log(new Date());
 };
-dynamicMethods["printTimestamp"] = function() {
+dynamicMethods["printTimestamp"] = function () {
   console.log(Date.now());
 };
 Object.keys(dynamicMethods).forEach(x => {
@@ -13,3 +13,17 @@ Object.keys(dynamicMethods).forEach(x => {
 });
 
 // understanding closures: functions & their lexical environment binded together
+
+// all the inner functions will retain all the lexical environments all the way through
+function adderfactory(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c
+    }
+  }
+}
+
+let adder5 = adderfactory(5);
+let adder5_9 = adder5(9);
+console.log(adder5_9(10))
+
